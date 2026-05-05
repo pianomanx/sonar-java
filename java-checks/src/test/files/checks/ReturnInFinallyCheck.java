4fbeb759560dd88152b3c481a71138b63cfb6444
Alban Auzeill <alban.auzeill@sonarsource.com>
2026-05-06 01:16:17 +0200

    SONARJAVA-6326 FP S1143: 'return' inside lambda in 'finally' block (#5593)

--- java-checks/src/test/files/checks/ReturnInFinallyCheck.java
+++ java-checks/src/test/files/checks/ReturnInFinallyCheck.java
@@ -70,6 +70,24 @@ enum A {
 
 }
 
+class LambdaInFinally {
+  void test() {
+    try {
+    } finally {
+      java.util.function.Predicate<String> p = s -> {
+        return s.isEmpty(); // Compliant - return exits the lambda, not the finally block
+      };
+      java.util.function.Supplier<String> supplier = () -> {
+        if (true) return "a"; // Compliant
+        for (int i = 0; i < 10; i++) {
+          return; // Compliant
+        }
+        return "b"; // Compliant
+      };
+    }
+  }
+}
+
 class FPs {
 
   int i;
